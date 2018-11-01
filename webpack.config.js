c03f69078012afbce79ecb6828a20be1d7a1324f
vano <kolesen.ivan@gmail.com>
2018-11-01 14:22:37 +0200

    build: set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "boundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  devtool: "source-map"
+};
